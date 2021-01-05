ffe147d13ca846731c327f37e884062120417d30
0xCleetusEth <69695266+0xCleetusEth@users.noreply.github.com>
2021-01-04 20:59:00 -0600

    comment clarification added

--- app.py
+++ app.py
@@ -5,11 +5,10 @@ from secrets import token_bytes
 
 from addrgen import read, write, keycall, leading_check
 
-# read the search.txt file and pass the contents to the var
+# read the search.txt file and pass the contents to the var, see modules "from addrgen import..."
 input_strs = read.readInputString()
 print(f'Checking for the following strings: {input_strs}')
 
-# State/module variable definitions - we are also starting the timer before entering the main loop.
 current_best_zeroes = 0
 count = 0
 timer = datetime.now().timestamp()
@@ -27,19 +26,16 @@ while True:
             write.writeHit(private_key, address)
             write.printHit(private_key, address)
 
-    # aforementioned 0 prefix checker.
     if leading_check.leading_zeroes(address) > current_best_zeroes:
         leading_check.leading_print(private_key, address)
         write.writeHit(private_key, address)
         current_best_zeroes = leading_check.leading_zeroes(address)
 
-    # Check for millions - if true, take time measurement and print results. Reset timer.
     if str(count).endswith('000000'):
         time_per_tick = datetime.now().timestamp() - timer
         timer = datetime.now().timestamp()
         write.time_print(time_per_tick, count)
 
-    # Add to the key-gen counter, display every 10,000.
     count += 1
     if str(count).endswith('0000'):
         write.print_progress(count)
